cdaff173681accc44302d68c8296e421c8866d31
Shivam Sadachar <shivamsoccer@gmail.com>
2019-06-15 17:42:06 -0700

    chore: added root reducer, added more actions

--- store.js
+++ store.js
@@ -4,9 +4,15 @@
 // 3. Listen to changes on the state
 // 4. Update the state
 
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
 //Example action
 {
-    type: "ADD_TODO",
+    type: ADD_TODO,
     todo: {
         id: 0,
         name: "Learn redux",
@@ -14,22 +20,55 @@
     }
 }
 
+{
+    type: REMOVE_TODO,
+    id: 0,
+  }
+  
+  {
+    type: TOGGLE_TODO,
+    id: 0,
+  }
+
+  {
+    type: ADD_GOAL,
+    goal: {
+      id: 0,
+      name: 'Run 5 miles per day'
+    }
+  }
+  
+  {
+    type: REMOVE_GOAL,
+    id: 0
+  }
 
 // Reducer function
 function todos (state = [], action) {
-    if (action.type === 'ADD_TODO') {
+    switch(action.type) {
+    case 'ADD_TODO':
       return state.concat([action.todo])
-    }
-    else if(action.type === 'REMOVE_TODO') {
+    case: REMOVE_TODO
         return state.filter( (todo) => todo.id !== action.id )
-    }
-    else if(action.type === 'TOGGLE_TODO') {
-      return state.map( (todo) => todo.id !== action.id ? todo : Object.assign({}, todo, {complete: !todo.complete }) )
-  }
-  
-    return state
+   case:TOGGLE_TODO
+      return state.map( (todo) => todo.id !== action.id ? todo : Object.assign({}, todo, {complete: !todo.complete }))
+    default:
+        return state
   }
 
+  function goals(state = [], action) {
+      switch(action.type) {
+          case ADD_GOAL:
+            return state.concat([action.goal])
+      }
+  }
+
+function app(state = {}, action) {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
 
 function createStore(reducer) {
     let state
@@ -37,7 +76,6 @@ function createStore(reducer) {
 
     const getState = () => state
 
-  
 
     const subscribe = (listener) => {
         listeners.push(listener)
@@ -48,7 +86,6 @@ function createStore(reducer) {
 
     const dispatch = (action) => {
         state = reducer(state, action);
-        debugger;
         listeners.forEach((listener) => listener());
     }
     
@@ -59,4 +96,4 @@ function createStore(reducer) {
     }
 }
 
-const store = createStore(todos)
+const store = createStore(app)
